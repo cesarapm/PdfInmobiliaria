9606bac6821b86335b4616578c101c05b4113dfa
cesarapm <garyfeomm@gmail.com>
2024-04-10 17:47:23 -0600

    Borreo las libreria htpds

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [{
+      name: 'apigeoapmonline.com',
+      script: './index.js',
+      node_args: ['--no-deprecation'], // Agrega esta línea
+      // Otros parámetros de configuración...
+    }],
+    // Otras configuraciones...
+  };
