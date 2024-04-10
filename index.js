851650e6388daa48911ce055491e14c92a49d6a0
cesarapm <garyfeomm@gmail.com>
2024-04-09 18:49:50 -0600

    actualize puerto 89

--- index.js
+++ index.js
@@ -60,6 +60,6 @@ app.get("/imagenes", (req, res) => {
   });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.listen(port, () => console.log(`Listening to port ${port}`));
